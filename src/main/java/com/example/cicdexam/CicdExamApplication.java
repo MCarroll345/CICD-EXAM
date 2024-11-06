f2a46523afb4fabbf8ea1a018a0dc2225d3a7f4f
Mark Carroll <G00419267@atu.ie>
2024-11-06 13:56:58 +0000

    Initialize

---
+++ src/main/java/com/example/cicdexam/CicdExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdExamApplication.class, args);
+    }
+
+}
